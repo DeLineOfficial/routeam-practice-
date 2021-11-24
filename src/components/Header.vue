a9f3f5dbdbbe492e77ab8413ef2fa30fa5989e61
Artem <artem.stolyar.2002@mail.ru>
2021-11-24 11:54:18 +0300

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+	<header>
+		<img src="../assets/logo.png" id="logo">
+		<div class="header_elements">
+			<div id="burger">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<div class="header_elements-adress">
+				<a href="/sales">Продажи</a>
+				<a href="#">Маршруты</a>
+			</div>
+		</div>
+		<div class="profile">
+			<img src="../assets/profile-logo.jpg">
+		</div>
+	</header>
+</template>
+<script type="text/javascript"></script>
+<style type="text/css">
+	header {
+		color: white;
+		height: 50px;
+		width: 100%;
+		background: black;
+		padding: 0 15px;
+		display: flex;
+		align-items: center;
+	}
+	#logo {
+		height: 100%;
+	}
+	.header_elements {
+		margin-left: 15px;
+		align-items: center;
+		display: flex;
+	}
+	#burger {
+		cursor: pointer;
+		display: flex;
+		flex-direction: column;
+		width: 30px;
+		height: 30px;
+		padding: 5px;
+	}
+	#burger > div {
+		align-items: center;
+		border-radius: 10px;
+		margin: 2px;	
+		width: 20px;
+		height: 2px;
+		background: white;
+	}
+	.header_elements > .header_elements-adress > a {
+		text-decoration: none;
+		color: white;
+		margin-left: 15px;
+		font-size: 18px;
+	}
+	.profile {
+		margin-left: auto;
+		display: flex;
+		align-items: center;
+		height: 100%;
+	}
+	.profile > img {
+		cursor: pointer;
+		object-fit: cover;	
+		width: 40px;
+		height: 40px;
+		border-radius: 50px;
+	}
+</style>
---
+++ src/views/Sales.vue
@@ -0,0 +1,13 @@
+<template>
+	<Header></Header>
+</template>
+<script type="text/javascript">
+	import Header from '@/components/Header.vue';
+
+	export default {
+		name: 'Sales',
+		components: {
+			Header,
+		}
+	}
+</script>
